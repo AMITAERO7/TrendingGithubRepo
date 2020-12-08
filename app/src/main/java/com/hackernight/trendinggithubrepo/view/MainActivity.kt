bef38c87b33c359ae1151746cb712d2aa21d0113
amitkumar <amitaero7@gmail.com>
2020-12-08 17:12:40 +0530

    Added Recycler View

--- app/src/main/java/com/hackernight/trendinggithubrepo/view/MainActivity.kt
+++ app/src/main/java/com/hackernight/trendinggithubrepo/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hackernight.trendinggithubrepo
+package com.hackernight.trendinggithubrepo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hackernight.trendinggithubrepo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
